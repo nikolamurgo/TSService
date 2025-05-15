60ec97398cb6fbfab7c8879a7d7dc15ed3125757
Nikola Murgovski <murgovskinikola@gmail.com>
2025-05-16 00:14:47 +0200

    db connection establish

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
 
+const db = require('./db/connection')
+
 
 app.use(cors());
 app.use(express.json());
@@ -13,12 +15,15 @@ app.get('/', (req, res) => {
   res.send('Backend is running');
 })
 
-app.get('/api/testdb', (req,res) =>{
-    db.query('SELECT 1 + 1 AS result', (err,result) =>{
-        if (err) return res.status(500).json({error: err.message})
-            res.json({dbTestResult: result[0].result})
-    })
-})
+app.get('/api/users', (req, res) => {
+  db.query('SELECT * FROM User', (err, results) => {
+    if (err) {
+      console.error('Error fetching users:', err);
+      return res.status(500).json({ error: 'Database error' });
+    }
+    res.json(results);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://88.200.63.148:${PORT}`);
