d27a416491000fbe9e7ae96f41c66e4edfd50d9c
Nikola Murgovski <murgovskinikola@gmail.com>
2025-05-20 19:44:35 +0200

    header modification styles

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,15 @@ import '../styles/HeaderStyles.css'
 function Header() {
   return (
     <div className="header-container">
-      <nav className="navbar bg-body-tertiary">
+      <nav className="navbar">
         <div className="container-fluid">
-          <a className="navbar-brand " href='#'>Dashboard</a>
+          <a className="navbar-brand" href='#'>Dashboard</a>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success" type="submit">Search</button>
+            <button className="btn btn-dark navbuttons" type="submit">Logout</button>
+            <button className="btn btn-dark navbuttons" type="submit">Account</button>
           </form>
-          <button className="btn btn-dark " type="submit">SignIn</button>
         </div>
       </nav>
     </div>
