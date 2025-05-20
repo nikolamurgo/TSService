3ce5b3bf91c3704c4be97640ff1016b565e44eb3
Nikola Murgovski <murgovskinikola@gmail.com>
2025-05-20 19:45:34 +0200

    adding links to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,11 +5,14 @@ import Logo from '../assets/tss-PNG.png'
 function Sidebar() {
     return (
         <div className='sidebar-container'>
-            <img src={Logo} width='100px'/>
+            <img src={Logo} width='100px' alt='logo'/>
             <span>TSService</span>
             <nav>
                 <ul>
                     <li className='nav-item'><Link to="/" className='nav-link text-white'>Dashboard</Link></li>
+                    <li className='nav-item'><Link to="/" className='nav-link text-white'>Add Repair Record</Link></li>
+                    <li className='nav-item'><Link to="/" className='nav-link text-white'>My Repairs</Link></li>
+                    <li className='nav-item'><Link to="/" className='nav-link text-white'>My Statistics</Link></li>
                 </ul>
             </nav>
         </div>
