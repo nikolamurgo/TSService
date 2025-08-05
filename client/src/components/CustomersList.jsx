04290746dba9855b38b902516742282652ca2a4b
Nikola Murgovski <murgovskinikola@gmail.com>
2025-08-05 03:26:54 +0200

    customers list component and page init

---
+++ client/src/components/CustomersList.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+function CustomersList(){
+
+    return(
+        <div>
+            {/* tbi*/}
+        </div>
+    )
+}
+
+export default CustomersList
---
+++ client/src/pages/Customers.jsx
@@ -0,0 +1,12 @@
+import CustomersList from '../components/CustomersList'
+
+function Customers(){
+    return(
+        <div>
+            <h2>My customers list.. TBI :)</h2>
+            <CustomersList />
+        </div>
+    )
+}
+
+export default Customers
