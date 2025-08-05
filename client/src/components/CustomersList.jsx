20eb0c23f240cbafc2bda857cf97698aa23e1df9
Nikola Murgovski <murgovskinikola@gmail.com>
2025-08-05 22:07:53 +0200

    fetch customers info from server

--- client/src/components/CustomersList.jsx
+++ client/src/components/CustomersList.jsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function CustomersList(){
+    const [customers, setCustomers] = useState([])
+    const [error, setError] = useState(null)
 
+    useEffect(() =>{
+        const fetchCustomers = async () => {
+            try{
+                const response = await axios.get("http://88.200.63.148:6060/api/customers")
+                setCustomers(response.data)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchCustomers()
+    }, [])
+    
     return(
         <div>
             {/* tbi*/}
