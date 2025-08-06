19985846d5802b1a8724c11bf63767029c9d70c9
Nikola Murgovski <murgovskinikola@gmail.com>
2025-08-07 01:03:27 +0200

    add links to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,6 +3,8 @@ import '../styles/SidebarStyles.css'
 import Logo from '../assets/tss-PNG.png'
 
 function Sidebar() {
+    const userRole = localStorage.getItem('role')
+
     return (
         <div className='sidebar-container'>
             <img src={Logo} width='100px' alt='logo'/>
@@ -15,6 +17,13 @@ function Sidebar() {
                     <li className='nav-item'><Link to="/customers" className='nav-link text-white'>My Customers</Link></li>
                     <li className='nav-item'><Link to="/inventory" className='nav-link text-white'>Inventory</Link></li>
                     <li className='nav-item'><Link to="/" className='nav-link text-white'>My Statistics</Link></li>
+                    {userRole === 'administrator' &&(
+                        <>
+                        <hr className="text-white" />
+                        <li className='nav-item'><Link to="/productivity" className='nav-link text-white'>Productivity</Link></li>
+                        <li className='nav-item'><Link to="/add-new-user" className='nav-link text-white'>Add New User</Link></li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </div>
