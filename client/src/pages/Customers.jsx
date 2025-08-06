cdd594c46a77774913323218ba66ea3e86ab245a
Nikola Murgovski <murgovskinikola@gmail.com>
2025-08-07 00:10:25 +0200

    cleanup

--- client/src/pages/Customers.jsx
+++ client/src/pages/Customers.jsx
@@ -3,7 +3,7 @@ import CustomersList from '../components/CustomersList'
 function Customers(){
     return(
         <div>
-            <h2>My customers list.. TBI :)</h2>
+            <h2>My customers list</h2>
             <CustomersList />
         </div>
     )
