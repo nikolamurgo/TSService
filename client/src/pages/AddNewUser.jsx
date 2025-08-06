6fca3bce0b410c3bc2ffaa7c772a2c888f8d7512
Nikola Murgovski <murgovskinikola@gmail.com>
2025-08-07 01:01:15 +0200

    initialize new pages for admin mode only

---
+++ client/src/pages/AddNewUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AddNewUser(){
+    return(
+        <div>
+            <h2>Add New User</h2>
+            //tbi
+        </div>
+    )
+}
+
+export default AddNewUser
---
+++ client/src/pages/Productivity.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Productivity(){
+    return(
+        <div>
+            <h2>Productivity dashboard</h2>
+            // tbi
+        </div>
+    )
+}
+
+export default Productivity
