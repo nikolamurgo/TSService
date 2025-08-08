7cef0eff7485933057c6873c882b409f0f82a013
Nikola Murgovski <murgovskinikola@gmail.com>
2025-08-09 00:59:46 +0200

    add component to page

--- client/src/pages/ManageUsers.jsx
+++ client/src/pages/ManageUsers.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import UsersList from '../components/UsersList'
 
 function ManageUsers(){
     return(
         <div>
-            <h2>tbi</h2>
+            <h2>Manage Users</h2>
+            <UsersList />
         </div>
     )
 }
