af77081282cf41e7e942f497f0dd33066301dd40
Nikola Murgovski <murgovskinikola@gmail.com>
2025-08-09 23:08:09 +0200

    myrepairs page displaying component list of repairs for specific user

---
+++ client/src/pages/MyRepairs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyRepairsList from '../components/MyRepairsList'
+
+function MyRepairs(){
+    return(
+        <div>
+            <MyRepairsList />
+        </div>
+    )
+}
+
+export default MyRepairs
