65500eaa90aa5ea97317db867c12e8e9830d6dfc
Nikola Murgovski <murgovskinikola@gmail.com>
2025-08-09 20:56:52 +0200

    fix post by using query to make transaction for posting

--- server/routes/repairParts.js
+++ server/routes/repairParts.js
@@ -9,7 +9,7 @@ router.get('/:recordId/parts', async (req,res) =>{
             SELECT rp.rp_id, rp.quantity_used, si.part_id, si.part_name, si.unit_price
             FROM RepairPart rp
             JOIN StockItem si ON rp.part_id = si.part_id
-            WHERE rp.record_id= ?`,
+            WHERE rp.repair_id= ?`,
         [recordId])
         res.json(parts)
     }catch(err){
@@ -21,40 +21,36 @@ router.post('/:recordId/parts', async(req,res) =>{
     const {recordId} = req.params
     const {part_id, quantity_used} = req.body
 
-
-    const connection = await db.promise().getConnection()
     try{
-        await connection.beginTransaction()
+        await db.promise().query('START TRANSACTION')
 
-        const [stock] = await connection.query(`
+        const [stock] = await db.promise().query(`
             SELECT quantity_available FROM StockItem
             WHERE part_id = ?`,
         [part_id])
 
         if(stock.length === 0 || stock[0].quantity_available < quantity_used){
-            await connection.rollback()
-            return res.json({message: "Not enough stock!"})
+            await db.promise().query('ROLLBACK')
+            return res.status(400).json({message: "Not enough stock!"})
         }
 
-        await connection.query(`
-            INSERT INTO RepairPart (record_id, part_id, quantity_used)
+        await db.promise().query(`
+            INSERT INTO RepairPart (repair_id, part_id, quantity_used)
             VALUES (?, ?, ?)`,
         [recordId, part_id, quantity_used ])
 
-        await connection.query(`
+        await db.promise().query(`
             UPDATE StockItem
             SET quantity_available = quantity_available - ?
             WHERE part_id = ?`,
         [quantity_used, part_id])
 
-        await connection.commit()
+        await db.promise().query('COMMIT')
         res.json({message: 'part added to repair recor'})
     }catch(err){
-        res.json({message: 'failed to add part to record'})
-    }finally{
-        connection.release()
+        await db.promise().query('ROLLBACK')
+        res.status(500).json({message: 'failed to add part to record'})
     }
-
 })
 
 module.exports = router
